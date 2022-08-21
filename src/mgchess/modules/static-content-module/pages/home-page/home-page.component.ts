17f5d64a3d279d3d7a8bc414f6ad31c9865d1929
Milosz08 <gilgamilosz451@gmail.com>
2022-08-21 22:57:36 +0200

    initialised base application page components

---
+++ src/mgchess/modules/static-content-module/pages/home-page/home-page.component.ts
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2022 by MILOSZ GILGA <https://miloszgilga.pl>
+ *
+ * File name: home-page.component.ts
+ * Last modified: 21/08/2022, 22:24
+ * Project name: chess-app-frontend
+ *
+ * Licensed under the MIT license; you may not use this file except in compliance with the License.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN ALL
+ * COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE.
+ */
+
+import { Component } from "@angular/core";
+
+//----------------------------------------------------------------------------------------------------------------------
+
+@Component({
+    selector: "mgchess-home-page",
+    templateUrl: "./home-page.component.html",
+    styleUrls: [ "./home-page.component.scss" ],
+})
+export class HomePageComponent {}
