b7b73b3ff4a4f10492f306860bb6a537eb2474f6
Milosz08 <gilgamilosz451@gmail.com>
2022-08-23 19:21:24 +0200

    create main application header (on static pages)

---
+++ src/mgchess/modules/static-content-module/components/header-with-navigation/header-with-navigation.component.ts
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2022 by MILOSZ GILGA <https://miloszgilga.pl>
+ *
+ * File name: header-with-navigation.component.ts
+ * Last modified: 22/08/2022, 17:10
+ * Project name: chess-app-frontend
+ *
+ * Licensed under the MIT license; you may not use this file except in compliance with the License.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN ALL
+ * COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE.
+ */
+
+import { Component } from "@angular/core";
+
+import { MainNavigationLinksModel } from "../../models/MainNavigationLinksModel";
+import * as NAVIGATION_LIST from "../../../../../static-data/main-navigation-links.json";
+
+//----------------------------------------------------------------------------------------------------------------------
+
+@Component({
+    selector: "mgchess-header-with-navigation",
+    templateUrl: "./header-with-navigation.component.html",
+    styleUrls: [ "./header-with-navigation.component.scss" ]
+})
+export class HeaderWithNavigationComponent {
+
+    readonly _mainNavigationList: Array<MainNavigationLinksModel>;
+
+    constructor() {
+        this._mainNavigationList = (NAVIGATION_LIST as any).default;
+    };
+}
