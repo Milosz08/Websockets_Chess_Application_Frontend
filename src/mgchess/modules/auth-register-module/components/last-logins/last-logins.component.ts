ce74ffc36dfdaf1c7e8df8474e4a599e8801eb62
Milosz08 <gilgamilosz451@gmail.com>
2022-09-12 03:48:18 +0200

    create alternative text, when remember accounts container is empty

--- src/mgchess/modules/auth-register-module/components/last-logins/last-logins.component.ts
+++ src/mgchess/modules/auth-register-module/components/last-logins/last-logins.component.ts
@@ -27,6 +27,8 @@ import { Component } from "@angular/core";
 })
 export class LastLoginsComponent {
 
+    _rememberAccounts: Array<String> = new Array<String>();
+
     handleRemoveAllRememberAccounts(): void {
         // TODO: remove all accounts from local storage
     };
