053a8655301f55a56f46a55a5a14df4f8638a7e7
Milosz08 <gilgamilosz451@gmail.com>
2022-10-03 19:16:49 +0200

    create activate-account NG page component

---
+++ src/mgchess/modules/static-content-module/pages/activate-account-page/activate-account-page.component.ts
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2022 by MILOSZ GILGA <https://miloszgilga.pl>
+ *
+ * File name: activate-account-page.component.ts
+ * Last modified: 03/10/2022, 12:04
+ * Project name: chess-app-frontend
+ *
+ * Licensed under the MIT license; you may not use this file except in compliance with the License.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN ALL
+ * COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE.
+ */
+
+import { Component } from "@angular/core";
+import { Meta, Title } from "@angular/platform-browser";
+
+import { BrowserMetaSerializatorLoader } from "../../../../browser-meta-serialization/browser-meta-serializator.loader";
+import { SingleModuleType, SinglePageType } from "../../../../browser-meta-serialization/browser-meta-serializator.types";
+
+//----------------------------------------------------------------------------------------------------------------------
+
+@Component({
+    selector: "mgchess-successful-activate-account",
+    templateUrl: "./activate-account-page.component.html",
+    host: { class: "mg-chess__flex-safety-container" },
+})
+export class ActivateAccountPageComponent extends BrowserMetaSerializatorLoader {
+
+    constructor(
+        private _metaService: Meta,
+        private _titleService: Title,
+    ) {
+        super(_titleService, _metaService, SingleModuleType.STATIC_CONTENT_MODULE,
+            SinglePageType.ACTIVATE_ACCOUNT_VIA_EMAIL_PAGE);
+    };
+}
